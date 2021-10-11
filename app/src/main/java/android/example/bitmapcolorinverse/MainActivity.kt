5db0a75659c8744e955890e6322c7bb5155b4ca7
alexander7dvorak <alexander7dvorak@gmail.com>
2021-10-11 15:17:04 +0300

    Changed implementation from assyncTask to looper/handler

--- app/src/main/java/android/example/bitmapcolorinverse/MainActivity.kt
+++ app/src/main/java/android/example/bitmapcolorinverse/MainActivity.kt
@@ -3,28 +3,25 @@ package android.example.bitmapcolorinverse
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 import java.io.InputStream
-import android.widget.ProgressBar
 import android.graphics.Bitmap
-import android.os.AsyncTask
-import android.view.View
-
+import android.os.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var bitmap:Bitmap? = null
-    private var inversionTask:MyAsyncTask = MyAsyncTask()
+    var bitmap: Bitmap? = null
+    private val mainThreadHandler = Handler(Looper.getMainLooper())
+    private val inversionTask: LooperThread = LooperThread().apply { start() }
 
     // extension function to get a bitmap from assets
     @SuppressLint("ResourceType")
-    fun Context.assetsToBitmap(fileName:String):Bitmap?{
+    fun Context.assetsToBitmap(fileName: String): Bitmap? {
         return try {
-            val stream:InputStream = resources.openRawResource(R.drawable.vibrant_avatar)
+            val stream: InputStream = resources.openRawResource(R.drawable.vibrant_avatar)
             BitmapFactory.decodeStream(stream)
         } catch (e: IOException) {
             e.printStackTrace()
@@ -42,61 +39,53 @@ class MainActivity : AppCompatActivity() {
         }
 
         button.setOnClickListener {
-            inversionTask=MyAsyncTask()
-            inversionTask?.execute(bitmap)
+            val message: Message = Message()
+            inversionTask.handler?.handleMessage(message)
         }
     }
 
-    private inner class MyAsyncTask : AsyncTask<Bitmap?, Integer?, Bitmap>() {
-        override fun onPreExecute() {
-            progressBar.setVisibility(ProgressBar.VISIBLE)
-        }
+    inner class LooperThread : Thread() {
+        var handler: Handler? = null
 
-        override fun doInBackground(vararg bitmaps: Bitmap?): Bitmap? {
-            bitmap?.apply {
-                invertColors()?.apply {
-                    imageView2.setImageBitmap(this)
+        override fun run() {
+            Looper.prepare()
+            //handler = Looper.myLooper()?.let { Handler(it) }
+            handler = object : Handler(Looper.myLooper()!!) {
+                override fun handleMessage(msg: Message) {
+                    bitmap?.apply {
+                        invertColors()?.apply {
+                            imageView2.setImageBitmap(this)
+                        }
+                    }
                 }
             }
-            return bitmap
+            Looper.loop()
         }
+    }
 
-        // extension function to invert bitmap colors
-        fun Bitmap.invertColors(): Bitmap? {
-            val tempBitmap = Bitmap.createBitmap(
-                width,
-                height,
-                Bitmap.Config.ARGB_8888
-            )
-            publishProgress(Integer(20))
-            val matrixInvert = ColorMatrix().apply {
-                set(
-                    floatArrayOf(
-                        -1.0f, 0.0f, 0.0f, 0.0f, 255.0f,
-                        0.0f, -1.0f, 0.0f, 0.0f, 255.0f,
-                        0.0f, 0.0f, -1.0f, 0.0f, 255.0f,
-                        0.0f, 0.0f, 0.0f, 1.0f, 0.0f
-                    )
+    fun Bitmap.invertColors(): Bitmap? {
+        val tempBitmap = Bitmap.createBitmap(
+            width,
+            height,
+            Bitmap.Config.ARGB_8888
+        )
+        val matrixInvert = ColorMatrix().apply {
+            set(
+                floatArrayOf(
+                    -1.0f, 0.0f, 0.0f, 0.0f, 255.0f,
+                    0.0f, -1.0f, 0.0f, 0.0f, 255.0f,
+                    0.0f, 0.0f, -1.0f, 0.0f, 255.0f,
+                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f
                 )
-            }
-
-            val paint = Paint()
-            ColorMatrixColorFilter(matrixInvert).apply {
-                paint.colorFilter = this
-            }
-            publishProgress(Integer(50))
-
-            bitmap?.let { Canvas(tempBitmap).drawBitmap(it, 0f, 0f, paint) }
-            return tempBitmap
+            )
         }
 
-        override fun onProgressUpdate(vararg values: Integer?) {
+        val paint = Paint()
+        ColorMatrixColorFilter(matrixInvert).apply {
+            paint.colorFilter = this
         }
 
-        override fun onPostExecute(result: Bitmap) {
-            progressBar.visibility = View.GONE
-            progressBar.setVisibility(ProgressBar.INVISIBLE)
-        }
-
+        bitmap?.let { Canvas(tempBitmap).drawBitmap(it, 0f, 0f, paint) }
+        return tempBitmap
     }
 }
